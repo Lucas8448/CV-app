b41439f2529c6cdfc1cd13b147861e89c84ee4e9
Lucas Bateson <lucas.bateson@gmail.com>
2023-01-19 22:41:41 +0100

    CV v1

---
+++ CV-app/Languages.swift
@@ -0,0 +1,20 @@
+//
+//  Languages.swift
+//  CV-app
+//
+//  Created by Lucas William Bateson on 19/01/2023.
+//
+
+import SwiftUI
+
+struct Languages: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Languages_Previews: PreviewProvider {
+    static var previews: some View {
+        Languages()
+    }
+}
