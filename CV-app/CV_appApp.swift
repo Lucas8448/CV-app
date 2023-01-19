fe48a126daddc92caabc82269ad0044c8cde6478
Lucas Bateson <lucas.bateson@gmail.com>
2023-01-19 09:53:03 +0100

    Initial Commit

---
+++ CV-app/CV_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  CV_appApp.swift
+//  CV-app
+//
+//  Created by Lucas William Bateson on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CV_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
